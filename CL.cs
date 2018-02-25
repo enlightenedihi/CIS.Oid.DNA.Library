8e6c4757c5ca14e86545c866b41fe0d687065953
enlightenedihi <enlightenihi@live.com>
2018-02-25 08:47:49 -0800

    Add files via upload

---
+++ CL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS.Oid.DNA.Library
+{
+    
+}
+
+namespace NCL
+{
+    public interface IDNA               //  декларация интерфейса ДНК 
+    {
+        int DNA();
+    }
+
+    public class CL : IDNA
+    {
+        public int DNA()                //  реализация интерфейса ДНК 
+        {
+            return 123;
+        }
+    }
+}
